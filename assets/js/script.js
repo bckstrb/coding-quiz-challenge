1e92f1be44812443c99c8c025f19339673b6270e
bckstrb <rbccstrb@gmail.com>
2022-07-08 15:04:58 -0600

    possSteps

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
 //instructions
-
-//timer
+    //-need paragraph stating what to do
+    //-need event clicker to start quiz
 
 //set of questions with options for answers
-
-//promts if wrong
+    //-4-5 questions with muliple choice answers
+    //-does it need a score associated with the answer? 
+    //-need event clicker
+    //-prompts if wrong
+    //-a way to calculate score
 
 //submit high scores
+    //-submit button
 
 //local storage-highscores
+
+//timer
